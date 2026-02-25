80e34c02295950655c7466745aaf318b72617721
priteshdeb10 <priteshdeb69@gmail.com>
2026-02-25 10:34:29 +0530

    UC3:String Reverse

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,25 +1,22 @@
-public class PalindromeCheckerApp{
+public class PalindromeCheckerApp {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter a string: ");
+        System.out.print("Enter text to check: ");
         String input = sc.nextLine();
 
-        boolean isPalindrome = true;
-
-        for (int i = 0; i < input.length() / 2; i++) {
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
+        String reversed = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
         }
-
-        if (isPalindrome) {
-            System.out.println("\"" + input + "\" is a palindrome.");
+        if (input.equals(reversed)) {
+            System.out.println("Result: \"" + input + "\" is a palindrome.");
         } else {
-            System.out.println("\"" + input + "\" is not a palindrome.");
+            System.out.println("Result: \"" + input + "\" is NOT a palindrome.");
         }
 
         sc.close();
     }
 }
+
+
