cdf37c08513ccf3e6ca6607e467899c5ece07e69
priteshdeb10 <priteshdeb69@gmail.com>
2026-02-25 10:11:07 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,19 @@
 public class PalindromeCheckerApp{
     public static void main(String[] args){
-        System.out.println("Welcome to the Palindrome Checker Management System!");
-        System.out.println("Application Version: 1.0");
-        System.out.println("System initialized. Ready for future updates.");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        boolean isPalindrome = true;
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        if (isPalindrome) {
+            System.out.println("\"" + input + "\" is a palindrome.");
+        } else {
+            System.out.println("\"" + input + "\" is not a palindrome.");
+        }
+        sc.close();
     }
 }
